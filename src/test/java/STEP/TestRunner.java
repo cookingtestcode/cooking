e11e5f4c9508361d9acb3113f2ed5c3b7ba6f9ed
fatima <fatimaothmna798@gmail.com>
2025-05-06 22:52:39 +0300

    Feature is 3

--- src/test/java/STEP/TestRunner.java
+++ src/test/java/STEP/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/Custome_Profile.feature"},
+        features = {"src/test/resources/SchedulingandTaskManagement.feature"},
         glue = {"STEP"},
         plugin = {"pretty", "html:target/cucumber-reports.html"}
 )
