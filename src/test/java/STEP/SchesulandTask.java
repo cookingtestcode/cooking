593aa9d47fbfcdc3280769a1a71f7bb45b688bde
fatima <fatimaothmna798@gmail.com>
2025-05-06 23:29:48 +0300

    Feature is 3

--- src/test/java/STEP/SchesulandTask.java
+++ src/test/java/STEP/SchesulandTask.java
@@ -1,6 +1,5 @@
 package STEP;
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.*;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -14,7 +13,6 @@ public class SchesulandTask {
     @Given("the following chefs exist:")
     public void the_following_chefs_exist(io.cucumber.datatable.DataTable dataTable) {
         chefData = dataTable.asMaps();
-        chefData.clear();
     }
 
     @Given("a task of type {string}")
@@ -24,12 +22,25 @@ public class SchesulandTask {
 
     @When("the task is assigned to a chef")
     public void the_task_is_assigned_to_a_chef() {
-        assignedChefName = null;
+        int minLoad = Integer.MAX_VALUE;
+        String selectedChef = null;
+
+        for (Map<String, String> chef : chefData) {
+            List<String> expertise = Arrays.asList(chef.get("expertise").split(","));
+            int load = Integer.parseInt(chef.get("currentLoad"));
+
+            if (expertise.contains(taskType) && load < minLoad) {
+                minLoad = load;
+                selectedChef = chef.get("name");
+            }
+        }
+
+        assignedChefName = selectedChef;
     }
 
     @When("the task is assigned to {string}")
     public void the_task_is_assigned_to_specific(String chefName) {
-        assignedChefName = null;
+        assignedChefName = chefName;
     }
 
     @Then("the task should be assigned to {string}")
@@ -40,13 +51,11 @@ public class SchesulandTask {
 
     @Then("{string} should have the task in their task list")
     public void chef_should_have_the_task(String chefName) {
-        List<String> fakeTaskList = new ArrayList<>();
-        assertTrue(fakeTaskList.contains(taskType), "Task not found in chef's task list");
+        fail("Not implemented yet");
     }
 
     @Then("{string} should be notified about the task")
     public void chef_should_be_notified(String chefName) {
-        boolean notified = false;
-        assertTrue(notified, "Chef was not notified");
+        fail("Not implemented yet");
     }
 }
