b5c17cf14eedb5db9b3418eb578991ee3930fc9e
fatima-othman <144529489+fatima-othman@users.noreply.github.com>
2025-05-17 22:12:25 +0300

    Update InventoryManager.java

--- src/main/java/model/InventoryManager.java
+++ src/main/java/model/InventoryManager.java
@@ -11,11 +11,9 @@ public class InventoryManager {
     }
 
     public void setIngredientStock(String ingredient, int quantity) {
-        // If the ingredient is already in inventory, update its stock
         if (inventory.containsKey(ingredient)) {
             inventory.put(ingredient, inventory.get(ingredient) + quantity);
         } else {
-            // If the ingredient is not in inventory, add it
             inventory.put(ingredient, quantity);
         }
     }
@@ -26,5 +24,24 @@ public class InventoryManager {
 
     public boolean checkStock(String ingredient, int quantity) {
         return inventory.getOrDefault(ingredient, 0) >= quantity;
+    }
+
+    public boolean checkLowStockAndNotifyManager() {
+        boolean isLowStock = false;
+
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            if (entry.getValue() < 5) {
+                isLowStock = true;
+                System.out.println("Low stock alert for: " + entry.getKey() + " - Only " + entry.getValue() + " left.");
+            }
+        }
+
+        if (isLowStock) {
+            System.out.println("Notifying manager about low stock.");
+            return true;
+        }
+
+        System.out.println("All items have sufficient stock.");
+        return false;
     }
 }
