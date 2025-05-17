e4486198f4df92b8189463c24049cab72d74a21b
fatima-othman <144529489+fatima-othman@users.noreply.github.com>
2025-05-17 22:09:38 +0300

    Update CustomerProfile.java

--- src/main/java/model/CustomerProfile.java
+++ src/main/java/model/CustomerProfile.java
@@ -7,6 +7,56 @@ public class CustomerProfile {
     private String name;
     private String dietaryPreference;
     private String allergy;
+    private List<String> orderHistory; 
+
+    public CustomerProfile() {
+        this.name = "";
+        this.dietaryPreference = "";
+        this.allergy = "";
+        this.orderHistory = new ArrayList<>(); 
+    }
+
+    // Getter and Setter methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDietaryPreference() {
+        return dietaryPreference;
+    }
+
+    public void setDietaryPreference(String dietaryPreference) {
+        this.dietaryPreference = dietaryPreference;
+    }
+
+    public String getAllergy() {
+        return allergy;
+    }
+
+    public void setAllergy(String allergy) {
+        this.allergy = allergy;
+    }
+
+    public void addOrder(String item) {
+        this.orderHistory.add(item);
+    }
+
+    public List<String> getOrderHistory() {
+        return this.orderHistory;
+    }
+    package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerProfile {
+    private String name;
+    private String dietaryPreference;
+    private String allergy;
     private List<String> orderHistory;  // حفظ تاريخ الطلبات
 
     public CustomerProfile() {
@@ -48,4 +98,5 @@ public class CustomerProfile {
     public List<String> getOrderHistory() {
         return this.orderHistory;
     }
+}
 }
