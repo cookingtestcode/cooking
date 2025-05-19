eaa1c7411811efe8fa65d11c1b23531572d51094
fatima_othman <fatimaothman798@gmail.com>
2025-05-19 12:31:31 +0300

    تعديل coverage to customer profile

--- src/main/java/model/CustomerProfile.java
+++ src/main/java/model/CustomerProfile.java
@@ -83,7 +83,7 @@ public class CustomerProfile {
         return (List)(personalizedSuggestions.isEmpty() ? this.mealSuggestions : personalizedSuggestions);
     }
 
-    public Map<String, Integer> analyzeOrderTrends() {
+   /* public Map<String, Integer> analyzeOrderTrends() {
         Map<String, Integer> trends = new HashMap();
         Iterator var2 = this.orderHistory.iterator();
 
@@ -93,7 +93,7 @@ public class CustomerProfile {
         }
 
         return trends;
-    }
+    }*/
 
     public void addOrder(String meal) {
         if (meal != null && !meal.trim().isEmpty()) {
@@ -114,21 +114,21 @@ public class CustomerProfile {
 
     }
 
-    public void clearOrderHistory() {
+   /* public void clearOrderHistory() {
         this.orderHistory.clear();
-    }
+    }*/
 
-    public List<String> getRecommendedItems(Menu menu) {
+   /* public List<String> getRecommendedItems(Menu menu) {
         return menu.getFilteredMenu(this);
     }
-
+*/
     public boolean hasAllergy(String ingredient) {
         return this.allergy != null && ingredient != null && ingredient.toLowerCase().contains(this.allergy.toLowerCase());
     }
 
-    public void rateMenuItem(String itemName, int rating) {
+    /*public void rateMenuItem(String itemName, int rating) {
         System.out.println("Customer " + this.name + " rated " + itemName + " with " + rating + " stars");
-    }
+    }*/
     private LocalDateTime mealDeliveryTime;
 
     public void scheduleMeal(LocalDateTime deliveryTime) {
